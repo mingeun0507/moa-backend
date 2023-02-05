9987748888808573228f9cae5546caad593ee5fc
mingeun0507 <tqdlfkg@naver.com>
2023-02-06 01:38:20 +1100

    initial commit

---
+++ src/test/java/com/hanamja/moa/MoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanamja.moa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
