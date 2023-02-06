07358d9d5075d2644633885f89d895ba06f01ffe
seungmin Ryu <ojysep9987@naver.com>
2023-02-06 18:21:37 +0900

    Feat: SpringSecurity Config 클래스 init
    "AWS CodeDeploy 서버 체크용 때문에 init 세팅 추가"

---
+++ src/main/java/com/hanamja/moa/config/SecurityConfig.java
@@ -0,0 +1,107 @@
+package com.hanamja.moa.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity // 기본 웹보안 사용
+@RequiredArgsConstructor
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfig {
+
+    /*
+     * AuthenticationManager를 주입받기 위해서 빈으로 등록한다.
+     * */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    // h2 database 테스트가 원활하도록 관련 API 들은 전부 무시
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/v*/api-docs",  "/configuration/ui",
+                        "/swagger-resources", "/configuration/security", "/swagger*/**",
+                        "/swagger-ui.html", "/webjars/**","/swagger/**")
+                .antMatchers("/h2-console/**", "/favicon.ico");
+    }
+
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .formLogin().disable();
+
+        return httpSecurity
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
+
+//                .authenticationProvider(customEmailPasswordAuthProvider)
+
+                .csrf().disable()// CSRF 설정 Disable
+                // exception handling 할 때 우리가 만든 클래스를 추가
+                .exceptionHandling()
+//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+//                .accessDeniedHandler(jwtAccessDeniedHandler)
+                /* iframe 관련 설정이고 X-frame-Options Click Jaking 공격을 기본적으로 막는걸로 설정되어있는데
+                 이를 풀기위한 설정을 하려면 아래의 설정을 추가하면 됨 */
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 시큐리티는 기본적으로 세션을 사용
+                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정\
+                .and()
+                .authorizeRequests() // http servletRequest 를 사용하는 요청들에 대한 접근제한을 설정
+                .antMatchers("/health/check").permitAll()
+                .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
+
+                // JwtFilter 를 등록한다.
+                // UsernamePasswordAuthenticationFilter 앞에 등록하는 이유는 딱히 없지만
+                // SecurityContext를 사용하기 때문에 앞단의 필터에서 SecurityContext가 설정되고 난뒤 필터를 둔다.
+                .and()
+//                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(false); // 쿠키를 받을건지
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+    }
+}
