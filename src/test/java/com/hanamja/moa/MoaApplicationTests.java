41f18ee8ef93ca5b28d09db751de0523e5c4063d
seungmin Ryu <ojysep9987@naver.com>
2023-02-06 14:24:37 +0900

    Feat: TEST 코드 해제
    "빌드 테스트 문제도 주석처리함"

--- src/test/java/com/hanamja/moa/MoaApplicationTests.java
+++ src/test/java/com/hanamja/moa/MoaApplicationTests.java
@@ -1,13 +1,13 @@
-package com.hanamja.moa;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MoaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.hanamja.moa;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MoaApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
