63d757b9b86370216533c2c1b93fb39621a53139
mingeun0507 <tqdlfkg@naver.com>
2023-02-08 04:34:24 +1100

    feat: User에 update 메소드 추가

--- src/main/java/com/hanamja/moa/api/entity/user/User.java
+++ src/main/java/com/hanamja/moa/api/entity/user/User.java
@@ -1,5 +1,9 @@
 package com.hanamja.moa.api.entity.user;
 
+import com.hanamja.moa.api.entity.album.Album;
+import com.hanamja.moa.api.entity.department.Department;
+import com.hanamja.moa.api.entity.group.Group;
+import com.hanamja.moa.api.entity.user_group.UserGroup;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -44,22 +48,40 @@ public class User implements UserDetails {
     @Column(name = "point")
     private Long point;
 
+    @Column(name = "intro")
+    private String intro;
+
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    // TODO: 학부 컬럼 추가 필요
+    @ManyToOne
+    @JoinColumn(name = "dept_id", nullable = false)
+    private Department department;
 
+    @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
+    private List<Album> albumList;
+
+    @OneToMany(mappedBy = "joiner", fetch = FetchType.LAZY)
+    private List<UserGroup> userGroupList;
+
+    @OneToMany(mappedBy = "maker", fetch = FetchType.LAZY)
+    private List<Group> groupList;
+
+//    @OneToMany(mappedBy = "metUser", fetch = FetchType.LAZY)
+//    private List<Album> metAlbumList;
 
     @Builder
-    public User(String studentId, String password, String name, String gender, String imageLink, Long point, Role role) {
+    public User(String studentId, String password, String name, String gender, String imageLink, Long point, String intro, Role role, Department department) {
         this.studentId = studentId;
         this.password = password;
         this.name = name;
         this.gender = gender;
         this.imageLink = imageLink;
         this.point = point;
+        this.intro = intro;
         this.role = role;
+        this.department = department;
     }
 
     @Override
@@ -105,5 +127,18 @@ public class User implements UserDetails {
     public boolean isFreshman() {
         String year = Integer.toString(Year.now().getValue());
         return this.studentId.startsWith(year);
+    }
+
+    // 온보딩용 업데이트 함수
+    public void updateOnboardingInfo(String gender, Department department, String imageLink) {
+        this.gender = gender;
+        this.department = department;
+        this.imageLink = imageLink == null ? this.imageLink : imageLink;
+    }
+
+    // 마이페이지 수정용 업데이트 함수
+    public void updateUserinfo(String imageLink, String intro) {
+        this.imageLink = imageLink == null ? this.imageLink : imageLink;
+        this.intro = intro == null ? this.intro : intro;
     }
 }
