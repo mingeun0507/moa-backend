37fa7733b9f6e04d85fbe5467d47f250d954ec09
mingeun0507 <tqdlfkg@naver.com>
2023-02-08 04:34:44 +1100

    feat: Album에 FK 추가 및 Builder 수정

--- src/main/java/com/hanamja/moa/api/entity/album/Album.java
+++ src/main/java/com/hanamja/moa/api/entity/album/Album.java
@@ -1,6 +1,8 @@
 package com.hanamja.moa.api.entity.album;
 
+import com.hanamja.moa.api.entity.user.User;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,15 +11,29 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "MOA_ALBUm")
+@Table(name = "MOA_ALBUM")
 public class Album {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "album_id")
     private Long id;
 
     @Column(name = "is_badged", nullable = false)
     private Boolean isBadged;
 
-    // TODO: 레퍼런스 키 추가 필요
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @ManyToOne
+    @JoinColumn(name = "met_user_id", nullable = false)
+    private User metUser;
+
+    @Builder
+    public Album(Boolean isBadged, User owner, User metUser) {
+        this.isBadged = isBadged;
+        this.owner = owner;
+        this.metUser = metUser;
+    }
 }
