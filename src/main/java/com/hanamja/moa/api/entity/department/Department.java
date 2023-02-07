900aa7497a42e3d8d49c2a8e3849ada1d6f4d922
mingeun0507 <tqdlfkg@naver.com>
2023-02-07 18:44:38 +1100

    feat: 학과 Entity 구현

---
+++ src/main/java/com/hanamja/moa/api/entity/department/Department.java
@@ -0,0 +1,23 @@
+package com.hanamja.moa.api.entity.department;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "MOA_DEPT")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dept_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+}
