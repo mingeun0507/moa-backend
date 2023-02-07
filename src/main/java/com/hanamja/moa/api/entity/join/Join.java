ff9634a0366ae0f67b3238dda949029f5f3352bf
mingeun0507 <tqdlfkg@naver.com>
2023-02-07 19:03:12 +1100

    feat: Join 엔티티 FK 제외 구현

---
+++ src/main/java/com/hanamja/moa/api/entity/join/Join.java
@@ -0,0 +1,27 @@
+package com.hanamja.moa.api.entity.join;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "MOA_JOIN")
+public class Join {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // TODO: 레퍼런스 키, 생성자 추가 필요
+
+    @Column(name = "join_at", nullable = false)
+    private LocalDateTime joinAt;
+
+    @Column(name = "progress", nullable = false)
+    private String progress;
+
+}
