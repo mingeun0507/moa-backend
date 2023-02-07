a4678ccf1703606cf653140118ce84db55121695
mingeun0507 <tqdlfkg@naver.com>
2023-02-07 19:00:04 +1100

    feat: Album 엔티티 FK 제외 구현

---
+++ src/main/java/com/hanamja/moa/api/entity/album/Album.java
@@ -0,0 +1,20 @@
+package com.hanamja.moa.api.entity.album;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "MOA_ALBUm")
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // TODO: 레퍼런스 키 추가 필요
+}
