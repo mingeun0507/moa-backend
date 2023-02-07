929ac3ed25aff64006589e10b1270a093dec9bff
mingeun0507 <tqdlfkg@naver.com>
2023-02-07 17:49:14 +1100

    feat: User 엔티티 정의

---
+++ src/main/java/com/hanamja/moa/api/entity/user/User.java
@@ -0,0 +1,94 @@
+package com.hanamja.moa.api.entity.user;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "MOA_USER")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "student_id", nullable = false, unique = true)
+    private String studentId;
+
+    @Column(name = "pwd", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    @Column(name = "image_link")
+    private String imageLink;
+
+    @Column(name = "point")
+    private Long point;
+
+    @Column(name = "is_freshmen",
+            columnDefinition = "Boolean GENERATED ALWAYS AS (IF(student_id like '23%', true, false))")
+    private Boolean isFreshmen;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // TODO: 학부 컬럼 추가 필요
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<String> roles = new ArrayList<>();
+        roles.add(this.role.toString());
+
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return studentId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
