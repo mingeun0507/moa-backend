61075c37a5a0943f7c4c65cbae429fec508b51c3
mingeun0507 <tqdlfkg@naver.com>
2023-02-08 21:22:00 +1100

    feat: GroupHashtagRepository 생성

---
+++ src/main/java/com/hanamja/moa/api/entity/group_hashtag/GroupHashtagRepository.java
@@ -0,0 +1,7 @@
+package com.hanamja.moa.api.entity.group_hashtag;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupHashtagRepository extends JpaRepository<GroupHashtag, Long> {
+}
