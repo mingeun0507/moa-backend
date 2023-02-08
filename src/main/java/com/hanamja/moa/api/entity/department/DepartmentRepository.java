12ff58c14634f4e15974a74140dec5e1cd9ccca2
mingeun0507 <tqdlfkg@naver.com>
2023-02-08 21:20:02 +1100

    feat: DepartmentRepository 생성

---
+++ src/main/java/com/hanamja/moa/api/entity/department/DepartmentRepository.java
@@ -0,0 +1,6 @@
+package com.hanamja.moa.api.entity.department;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
