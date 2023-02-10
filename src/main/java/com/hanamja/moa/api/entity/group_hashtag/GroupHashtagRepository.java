31e0a131219b89fb2b90faa0e2ba43d2257622b1
mingeun0507 <tqdlfkg@naver.com>
2023-02-11 02:14:05 +1100

    feat: Group 수정을 위한 GroupHashtagRepository 메소드 구현

--- src/main/java/com/hanamja/moa/api/entity/group_hashtag/GroupHashtagRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/group_hashtag/GroupHashtagRepository.java
@@ -4,4 +4,5 @@ package com.hanamja.moa.api.entity.group_hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupHashtagRepository extends JpaRepository<GroupHashtag, Long> {
+    void deleteAllByGroup_Id(Long groupId);
 }
