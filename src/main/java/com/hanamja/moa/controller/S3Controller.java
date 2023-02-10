f27b4374e3660f686af0f868fd055bf0502a400a
seungmin Ryu <ojysep9987@naver.com>
2023-02-10 17:00:25 +0900

    Feat: AWS S3 upload test Controller

---
+++ src/main/java/com/hanamja/moa/controller/S3Controller.java
@@ -0,0 +1,25 @@
+package com.hanamja.moa.controller;
+
+import com.hanamja.moa.service.S3UploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+public class S3Controller {
+    private final S3UploadService s3UploadService;
+
+    @PostMapping("/s3/upload")
+    public ResponseEntity<?> uploadFile(@RequestPart("images") MultipartFile multipartFile){
+        try {
+            String url = s3UploadService.saveFileAndGetUrl(multipartFile);
+            return ResponseEntity.ok().body(url);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
