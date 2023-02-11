c5db8b572e50a9e71abf41fba10f3a2d6e203972
mingeun0507 <tqdlfkg@naver.com>
2023-02-12 00:19:02 +1100

    feat: 메소드에 대한 에러 처리 구현 및 모듈화

--- src/main/java/com/hanamja/moa/api/service/group/GroupService.java
+++ src/main/java/com/hanamja/moa/api/service/group/GroupService.java
@@ -13,6 +13,8 @@ import com.hanamja.moa.api.entity.hashtag.HashtagRepository;
 import com.hanamja.moa.api.entity.user.User;
 import com.hanamja.moa.api.entity.user.UserRepository;
 import com.hanamja.moa.api.entity.user_group.UserGroupRepository;
+import com.hanamja.moa.exception.custom.GroupNotFoundException;
+import com.hanamja.moa.exception.custom.InvalidMaxPeopleNumberException;
 import com.hanamja.moa.exception.custom.UserInputException;
 import com.hanamja.moa.exception.custom.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -37,14 +39,8 @@ public class GroupService {
     @Transactional
     public GroupInfoResponseDto makeNewGroup(MakingGroupRequestDto makingGroupRequestDto) {
         // TODO: 로그인 구현 후 @AuthenticationPrincipal User user 추가 필요
-
-        User user = userRepository.findById(1L).orElseThrow(
-                () -> UserNotFoundException
-                        .builder()
-                        .httpStatus(HttpStatus.BAD_REQUEST)
-                        .message("유효하지 않은 사용자입니다.")
-                        .build()
-        );
+        Long userId = 1L;
+        User user = validateUser(userId);
 
         // 재학생인지 검증
         validateSenior(user);
@@ -65,6 +61,100 @@ public class GroupService {
         ).forEach(groupHashtagRepository::save);
 
         return GroupInfoResponseDto.from(newGroup);
+    }
+
+    @Transactional
+    public GroupInfoResponseDto modifyExistingGroup(ModifyingGroupRequestDto modifyingGroupRequestDto) {
+        // TODO: 로그인 구현 후 @AuthenticationPrincipal User user 추가 필요
+
+        Long userId = 1L;
+        User user = validateUser(userId);
+
+        // 재학생인지 검증
+        validateSenior(user);
+
+        // groupId로 group 찾아오기
+        Group existingGroup = groupRepository.findById(modifyingGroupRequestDto.getId()).orElseThrow(
+                () -> GroupNotFoundException
+                        .builder()
+                        .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .message("일시적 오류입니다. 다시 시도해주세요.")
+                        .build()
+        );
+
+        validateMaker(user, existingGroup);
+
+        if (existingGroup.getCurrentPeopleNum() > modifyingGroupRequestDto.getMaxPeopleNum()) {
+            throw InvalidMaxPeopleNumberException
+                    .builder()
+                    .httpStatus(HttpStatus.BAD_REQUEST)
+                    .message("모임에 신청한 인원 수보다 적게 줄일 수 없습니다.")
+                    .build();
+        }
+
+        // group 정보 수정
+        existingGroup.modifyGroupInfo(
+                modifyingGroupRequestDto.getName(),
+                modifyingGroupRequestDto.getDescription(),
+                modifyingGroupRequestDto.getMeetingAt(),
+                modifyingGroupRequestDto.getMaxPeopleNum()
+        );
+
+        // 기존 해시태그 정보 삭제
+        groupHashtagRepository.deleteAllByGroup_Id(existingGroup.getId());
+
+        // 새로운 해시태그 정보 등록 및 관계 테이블에 저장
+        List<Hashtag> hashtagList = saveHashtags(modifyingGroupRequestDto.getHashtags());
+        hashtagList.stream().map(
+                x -> GroupHashtag
+                        .builder()
+                        .group(existingGroup)
+                        .hashtag(x)
+                        .build()
+        ).forEach(groupHashtagRepository::save);
+
+        // 수정된 group 정보 반영
+        return GroupInfoResponseDto.from(groupRepository.save(existingGroup));
+    }
+
+    @Transactional
+    public GroupInfoResponseDto removeExistingGroup(RemovingGroupRequestDto removingGroupRequestDto) {
+        Long userId = 1L;
+        User user = validateUser(userId);
+
+        // 재학생인지 검증
+        validateSenior(user);
+
+        // groupId로 group 찾아오기
+        Group existingGroup = groupRepository.findById(removingGroupRequestDto.getId()).orElseThrow(
+                () -> GroupNotFoundException
+                        .builder()
+                        .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .message("일시적 오류입니다. 다시 시도해주세요.")
+                        .build()
+        );
+
+        GroupInfoResponseDto removedGroupDto = GroupInfoResponseDto.from(existingGroup);
+
+        // UserGroup, GroupHashtag에서 해당 Group 모두 삭제
+        userGroupRepository.deleteAllByGroup_Id(removingGroupRequestDto.getId());
+        groupHashtagRepository.deleteAllByGroup_Id(removingGroupRequestDto.getId());
+
+        // Group 삭제
+        groupRepository.deleteById(removingGroupRequestDto.getId());
+
+        return removedGroupDto;
+
+    }
+
+    private User validateUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(
+                () -> UserNotFoundException
+                        .builder()
+                        .httpStatus(HttpStatus.BAD_REQUEST)
+                        .message("유효하지 않은 사용자입니다.")
+                        .build()
+        );
     }
 
     private void validateSenior(User user) {
@@ -113,79 +203,5 @@ public class GroupService {
                     }
                 }
         ).collect(Collectors.toList());
-    }
-
-    @Transactional
-    public GroupInfoResponseDto modifyExistingGroup(ModifyingGroupRequestDto modifyingGroupRequestDto) {
-        // TODO: 로그인 구현 후 @AuthenticationPrincipal User user 추가 필요
-
-        User user = userRepository.findById(1L).orElseThrow(
-                // TODO: Exception 구현 후 사용자를 찾지 못한 경우 unchecked Exception 던지기
-        );
-
-        // 재학생인지 검증
-        validateSenior(user);
-
-        // groupId로 group 찾아오기
-        Group existingGroup = groupRepository.findById(modifyingGroupRequestDto.getId()).orElseThrow(
-                // TODO: group이 없을 때 400 Bad Request 던지도록 구현 필요
-        );
-
-        validateMaker(user, existingGroup);
-
-        if (existingGroup.getCurrentPeopleNum() > modifyingGroupRequestDto.getMaxPeopleNum()) {
-            // TODO: 새로운 group 최대 인원수보다 현재 인원수가 많으면 400 Bad Request
-        }
-
-        // group 정보 수정
-        existingGroup.modifyGroupInfo(
-                modifyingGroupRequestDto.getName(),
-                modifyingGroupRequestDto.getDescription(),
-                modifyingGroupRequestDto.getMeetingAt(),
-                modifyingGroupRequestDto.getMaxPeopleNum()
-        );
-
-        // 기존 해시태그 정보 삭제
-        groupHashtagRepository.deleteAllByGroup_Id(existingGroup.getId());
-
-        // 새로운 해시태그 정보 등록 및 관계 테이블에 저장
-        List<Hashtag> hashtagList = saveHashtags(modifyingGroupRequestDto.getHashtags());
-        hashtagList.stream().map(
-                x -> GroupHashtag
-                        .builder()
-                        .group(existingGroup)
-                        .hashtag(x)
-                        .build()
-        ).forEach(groupHashtagRepository::save);
-
-        // 수정된 group 정보 반영
-        return GroupInfoResponseDto.from(groupRepository.save(existingGroup));
-    }
-
-    @Transactional
-    public GroupInfoResponseDto removeExistingGroup(RemovingGroupRequestDto removingGroupRequestDto) {
-        User user = userRepository.findById(1L).orElseThrow(
-                // TODO: Exception 구현 후 사용자를 찾지 못한 경우 unchecked Exception 던지기
-        );
-
-        // 재학생인지 검증
-        validateSenior(user);
-
-        // groupId로 group 찾아오기
-        Group existingGroup = groupRepository.findById(removingGroupRequestDto.getId()).orElseThrow(
-                // TODO: group이 없을 때 400 Bad Request 던지도록 구현 필요
-        );
-
-        GroupInfoResponseDto removedGroupDto = GroupInfoResponseDto.from(existingGroup);
-
-        // UserGroup, GroupHashtag에서 해당 Group 모두 삭제
-        userGroupRepository.deleteAllByGroup_Id(removingGroupRequestDto.getId());
-        groupHashtagRepository.deleteAllByGroup_Id(removingGroupRequestDto.getId());
-
-        // Group 삭제
-        groupRepository.deleteById(removingGroupRequestDto.getId());
-
-        return removedGroupDto;
-
     }
 }
