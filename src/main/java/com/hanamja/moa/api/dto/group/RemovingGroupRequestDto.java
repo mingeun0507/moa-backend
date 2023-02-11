8742d0be3edefb75dd7253d0f02399132aa1da72
mingeun0507 <tqdlfkg@naver.com>
2023-02-11 22:30:27 +1100

    feat: 그룹 삭제 요청시 사용할 RequestDto 구현

--- src/main/java/com/hanamja/moa/api/dto/group/RemovingGroupRequestDto.java
+++ src/main/java/com/hanamja/moa/api/dto/group/RemovingGroupRequestDto.java
@@ -1,14 +1,8 @@
 package com.hanamja.moa.api.dto.group;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class RemovingGroupRequestDto {
     Long id;
-
-    @Builder
-    public RemovingGroupRequestDto(Long id) {
-        this.id = id;
-    }
 }
