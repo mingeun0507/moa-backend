b0c4d884b775e63663cd2a90b05a90b56812c11d
mingeun0507 <tqdlfkg@naver.com>
2023-02-11 22:30:49 +1100

    feat: Group 삭제 시 사용할 메소드 구현

--- src/main/java/com/hanamja/moa/api/entity/user_group/UserGroupRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/user_group/UserGroupRepository.java
@@ -3,4 +3,5 @@ package com.hanamja.moa.api.entity.user_group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
+    void deleteAllByGroup_Id(Long groupId);
 }
