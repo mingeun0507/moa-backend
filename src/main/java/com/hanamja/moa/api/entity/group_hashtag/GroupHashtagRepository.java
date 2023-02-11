3d657f4ced421a63167209b0c7d4fa656fbd41b2
mingeun0507 <tqdlfkg@naver.com>
2023-02-12 00:54:34 +1100

    feat: Hashtag를 불러오기 위한 메소드 추가

--- src/main/java/com/hanamja/moa/api/entity/group_hashtag/GroupHashtagRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/group_hashtag/GroupHashtagRepository.java
@@ -3,6 +3,11 @@ package com.hanamja.moa.api.entity.group_hashtag;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GroupHashtagRepository extends JpaRepository<GroupHashtag, Long> {
+
+    Optional<GroupHashtag> findAllByGroup_Id(Long groupId);
+
     void deleteAllByGroup_Id(Long groupId);
 }
