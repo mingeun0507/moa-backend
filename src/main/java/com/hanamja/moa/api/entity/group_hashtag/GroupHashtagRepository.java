b831dac5842b0423d8ae631ba647759d11e49987
mingeun0507 <tqdlfkg@naver.com>
2023-02-14 19:54:35 +1100

    fix: Optional에서 List로 수정

--- src/main/java/com/hanamja/moa/api/entity/group_hashtag/GroupHashtagRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/group_hashtag/GroupHashtagRepository.java
@@ -3,11 +3,11 @@ package com.hanamja.moa.api.entity.group_hashtag;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface GroupHashtagRepository extends JpaRepository<GroupHashtag, Long> {
 
-    Optional<GroupHashtag> findAllByGroup_Id(Long groupId);
+    List<GroupHashtag> findAllByGroup_Id(Long groupId);
 
     void deleteAllByGroup_Id(Long groupId);
 }
