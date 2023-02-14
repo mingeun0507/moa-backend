0e20aa755f874d0b3a45827b3eff20179b22853e
mingeun0507 <tqdlfkg@naver.com>
2023-02-14 19:55:39 +1100

    feat: 전체 그룹 조회 시 사용할 비즈니스 로직 메소드 구현

--- src/main/java/com/hanamja/moa/api/entity/group/GroupRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/group/GroupRepository.java
@@ -3,5 +3,11 @@ package com.hanamja.moa.api.entity.group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface GroupRepository extends JpaRepository<Group, Long> {
+    List<Group> findAllByStateOrderByCreatedAtDesc(State state);
+
+    List<Group> findAllByStateAndMeetingAtAfterOrderByMeetingAtAscCreatedAtDesc(State state, LocalDateTime currentTime);
 }
