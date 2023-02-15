d958b35496688bd68acd06da114fa17ba1b10260
mingeun0507 <tqdlfkg@naver.com>
2023-02-15 23:28:09 +1100

    feat: 그룹 상세 정보 조회 시 사용할 AlbumRepository 내 비즈니스 로직 구현

--- src/main/java/com/hanamja/moa/api/entity/album/AlbumRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/album/AlbumRepository.java
@@ -3,4 +3,5 @@ package com.hanamja.moa.api.entity.album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+    Boolean existsByOwner_IdAndMetUser_Id(Long ownerId, Long metUserId);
 }
