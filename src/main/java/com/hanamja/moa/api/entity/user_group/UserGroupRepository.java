0b739a6795f48696eb80891f88b19f1fa529bfb9
mingeun0507 <tqdlfkg@naver.com>
2023-02-15 23:28:35 +1100

    feat: 그룹 상세 정보 조회 시 사용할 비즈니스 로직 구현

--- src/main/java/com/hanamja/moa/api/entity/user_group/UserGroupRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/user_group/UserGroupRepository.java
@@ -2,6 +2,12 @@ package com.hanamja.moa.api.entity.user_group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
     void deleteAllByGroup_Id(Long groupId);
+
+    List<UserGroup> findAllByGroup_Id(Long groupId);
+
+    int countAllByJoiner_Id(Long userId);
 }
