bd2ad5beec2c84c7e1a99f47be5fb33613706804
mingeun0507 <tqdlfkg@naver.com>
2023-02-18 01:45:51 +1100

    feat: 삭제 요청 API 구현

--- src/main/java/com/hanamja/moa/api/controller/history/HistoryController.java
+++ src/main/java/com/hanamja/moa/api/controller/history/HistoryController.java
@@ -1,14 +1,12 @@
 package com.hanamja.moa.api.controller.history;
 
+import com.hanamja.moa.api.dto.history.request.RemovingHistoryRequestDto;
 import com.hanamja.moa.api.dto.history.response.HistoryDetailInfoResponseDto;
 import com.hanamja.moa.api.dto.history.response.HistoryInfoResponseDto;
 import com.hanamja.moa.api.service.history.HistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +28,12 @@ public class HistoryController {
         // TODO: 로그인 구현 후 @AuthenticationPrincipal User user 추가 필요
 
         return ResponseEntity.ok(historyService.getHistoryDetail(historyId));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HistoryDetailInfoResponseDto> removeHistory(@RequestBody RemovingHistoryRequestDto removingHistoryRequestDto) {
+        // TODO: 로그인 구현 후 @AuthenticationPrincipal User user 추가 필요
+
+        return ResponseEntity.ok(historyService.removeHistory(removingHistoryRequestDto));
     }
 }
