e5482140fa00a885e37c0d102db9cd40e3821b32
mingeun0507 <tqdlfkg@naver.com>
2023-02-18 01:00:57 +1100

    fix: AntMatcher 관련 오류 수정

--- src/main/java/com/hanamja/moa/config/SecurityConfig.java
+++ src/main/java/com/hanamja/moa/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
-                .antMatchers("");
+                .antMatchers("/**");
     }
 
 
