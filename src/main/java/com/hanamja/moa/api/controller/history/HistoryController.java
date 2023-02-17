22c8acbaf7ea9c5d4e3b6b0b32e58fb659156aac
mingeun0507 <tqdlfkg@naver.com>
2023-02-18 01:00:26 +1100

    feat: 내역 전체 조회 API 구현

---
+++ src/main/java/com/hanamja/moa/api/controller/history/HistoryController.java
@@ -0,0 +1,25 @@
+package com.hanamja.moa.api.controller.history;
+
+import com.hanamja.moa.api.dto.history.response.HistoryInfoResponseDto;
+import com.hanamja.moa.api.service.history.HistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/history")
+public class HistoryController {
+    private final HistoryService historyService;
+
+    @GetMapping
+    public ResponseEntity<List<HistoryInfoResponseDto>> getHistoryList() {
+        // TODO: 로그인 구현 후 @AuthenticationPrincipal User user 추가 필요
+
+        return ResponseEntity.ok(historyService.getHistoryList());
+    }
+}
