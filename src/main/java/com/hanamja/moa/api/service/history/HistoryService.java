e11fc32ee56586324911973dba7f4290dd8195a5
mingeun0507 <tqdlfkg@naver.com>
2023-02-18 01:17:55 +1100

    feat: 상세 정보 조회 API를 구현하기 위한 비즈니스 로직 구현

--- src/main/java/com/hanamja/moa/api/service/history/HistoryService.java
+++ src/main/java/com/hanamja/moa/api/service/history/HistoryService.java
@@ -1,11 +1,14 @@
 package com.hanamja.moa.api.service.history;
 
+import com.hanamja.moa.api.dto.history.response.HistoryDetailInfoResponseDto;
 import com.hanamja.moa.api.dto.history.response.HistoryInfoResponseDto;
 import com.hanamja.moa.api.entity.history.HistoryRepository;
 import com.hanamja.moa.api.entity.user.User;
 import com.hanamja.moa.api.entity.user.UserRepository;
+import com.hanamja.moa.exception.custom.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +22,7 @@ public class HistoryService {
     private final HistoryRepository historyRepository;
 
     public List<HistoryInfoResponseDto> getHistoryList() {
+        // TODO: 로그인 구현 후 @AuthenticationPrincipal User user 추가 필요
         User user = userRepository.findById(1L).orElseThrow();
 
         return historyRepository
@@ -26,5 +30,22 @@ public class HistoryService {
                 .stream()
                 .map(HistoryInfoResponseDto::from)
                 .collect(Collectors.toList());
+    }
+
+    public HistoryDetailInfoResponseDto getHistoryDetail(Long historyId) {
+        // TODO: 로그인 구현 후 @AuthenticationPrincipal User user 추가 필요
+        User user = userRepository.findById(1L).orElseThrow();
+
+        return HistoryDetailInfoResponseDto.from(historyRepository
+                .findById(historyId)
+                .orElseThrow(
+                        () -> NotFoundException
+                                .builder()
+                                .httpStatus(HttpStatus.BAD_REQUEST)
+                                .message("해당하는 historyId로 history를 찾을 수 없습니다.")
+                                .build()
+
+                )
+        );
     }
 }
