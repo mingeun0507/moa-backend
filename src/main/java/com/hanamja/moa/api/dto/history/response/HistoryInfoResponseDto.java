3d330f2647f43a1540e2eab22b14502d37a1cc39
mingeun0507 <tqdlfkg@naver.com>
2023-02-21 11:39:38 +0900

    feat: 포인트 적립 내역 ResponseDto 형식 변경

--- src/main/java/com/hanamja/moa/api/dto/history/response/HistoryInfoResponseDto.java
+++ src/main/java/com/hanamja/moa/api/dto/history/response/HistoryInfoResponseDto.java
@@ -14,22 +14,29 @@ public class HistoryInfoResponseDto {
     private Long id;
     private String title;
     private Long point;
+    private String cardMessage;
+    private String groupMessage;
     private LocalDateTime createdAt;
 
     @Builder
-    public HistoryInfoResponseDto(Long id, String title, Long point, LocalDateTime createdAt) {
+    public HistoryInfoResponseDto(Long id, String title, Long point, String cardMessage, String groupMessage, LocalDateTime createdAt) {
         this.id = id;
         this.title = title;
         this.point = point;
+        this.cardMessage = cardMessage;
+        this.groupMessage = groupMessage;
         this.createdAt = createdAt;
     }
 
     public static HistoryInfoResponseDto from(History history) {
+        String[] splitMessages = history.getMessage().split("\n");
         return HistoryInfoResponseDto
                 .builder()
                 .id(history.getId())
                 .title(history.getTitle())
                 .point(history.getPoint())
+                .cardMessage(splitMessages[0])
+                .groupMessage(splitMessages[1])
                 .createdAt(history.getCreatedAt())
                 .build();
     }
