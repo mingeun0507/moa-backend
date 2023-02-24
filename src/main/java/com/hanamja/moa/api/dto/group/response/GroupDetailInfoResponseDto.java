e6023a854ff7bb25906d35e9df0d892271f10081
mingeun0507 <tqdlfkg@naver.com>
2023-02-24 21:19:07 +0900

    fix: ResponseDto 수정

--- src/main/java/com/hanamja/moa/api/dto/group/response/GroupDetailInfoResponseDto.java
+++ src/main/java/com/hanamja/moa/api/dto/group/response/GroupDetailInfoResponseDto.java
@@ -1,7 +1,6 @@
 package com.hanamja.moa.api.dto.group.response;
 
 import com.hanamja.moa.api.entity.group.Group;
-import com.hanamja.moa.api.entity.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,17 +12,17 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GroupDetailInfoResponseDto {
     private GroupInfoResponseDto groupInfoResponseDto;
-    private List<SimpleUserInfoDto> userInfoDtoList;
+    private List<SimpleUserInfoResponseDto> userInfoDtoList;
     private int point;
 
     @Builder
-    public GroupDetailInfoResponseDto(GroupInfoResponseDto groupInfoResponseDto, List<SimpleUserInfoDto> userInfoDtoList, int point) {
+    public GroupDetailInfoResponseDto(GroupInfoResponseDto groupInfoResponseDto, List<SimpleUserInfoResponseDto> userInfoDtoList, int point) {
         this.groupInfoResponseDto = groupInfoResponseDto;
         this.userInfoDtoList = userInfoDtoList;
         this.point = point;
     }
 
-    public static GroupDetailInfoResponseDto from(Group group, List<String> hashtags, List<SimpleUserInfoDto> userInfoDtoList, int point) {
+    public static GroupDetailInfoResponseDto from(Group group, List<String> hashtags, List<SimpleUserInfoResponseDto> userInfoDtoList, int point) {
         return GroupDetailInfoResponseDto
                 .builder()
                 .groupInfoResponseDto(GroupInfoResponseDto.from(group, hashtags))
@@ -32,27 +31,4 @@ public class GroupDetailInfoResponseDto {
                 .build();
     }
 
-    @Getter
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    public static class SimpleUserInfoDto {
-        private Long id;
-        private String name;
-        private String imageLink;
-
-        @Builder
-        public SimpleUserInfoDto(Long id, String name, String imageLink) {
-            this.id = id;
-            this.name = name;
-            this.imageLink = imageLink;
-        }
-
-        public static SimpleUserInfoDto from(User user) {
-            return SimpleUserInfoDto
-                    .builder()
-                    .id(user.getId())
-                    .name(user.getName())
-                    .imageLink(user.getImageLink())
-                    .build();
-        }
-    }
 }
