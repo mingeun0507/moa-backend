8b690a028e29da39086305c8a0735c40e5539962
Jihyun <bjih1999@naver.com>
2023-02-24 21:00:55 +0900

    feat: User is_active, is_onboarded 추가

--- src/main/java/com/hanamja/moa/api/dto/auth/response/LoginResponseDto.java
+++ src/main/java/com/hanamja/moa/api/dto/auth/response/LoginResponseDto.java
@@ -12,10 +12,17 @@ public class LoginResponseDto {
 
     private String refreshToken;
 
-    public static LoginResponseDto of(String accessToken, String refreshToken) {
+    private boolean isOnBoarded;
+
+    private boolean isActive;
+
+
+    public static LoginResponseDto of(String accessToken, String refreshToken, boolean isOnBoarded, boolean isActive) {
         LoginResponseDto responseDto = new LoginResponseDto();
         responseDto.accessToken = accessToken;
         responseDto.refreshToken = refreshToken;
+        responseDto.isOnBoarded = isOnBoarded;
+        responseDto.isActive = isActive;
 
         return responseDto;
     }
