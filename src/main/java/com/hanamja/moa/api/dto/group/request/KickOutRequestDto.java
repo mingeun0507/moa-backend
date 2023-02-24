9a22cb9c79bf96b9cc7687f3121db451d4bc51c5
mingeun0507 <tqdlfkg@naver.com>
2023-02-25 01:31:09 +0900

    fix: @JsonProperty 어노테이션 삭제

--- src/main/java/com/hanamja/moa/api/dto/group/request/KickOutRequestDto.java
+++ src/main/java/com/hanamja/moa/api/dto/group/request/KickOutRequestDto.java
@@ -1,7 +1,5 @@
 package com.hanamja.moa.api.dto.group.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,14 +10,13 @@ import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class KickOutRequestDto {
 
     @NotNull
-    @JsonProperty(value = "group_id")
     private Long groupId;
 
     @NotNull
-    @JsonProperty(value = "user_list")
     private List<Long> userList;
 
     @NotNull
