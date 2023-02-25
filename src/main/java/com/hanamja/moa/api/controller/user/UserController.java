1ac00083a0b785926f342e4f52fddb3448acd731
Jihyun <bjih1999@naver.com>
2023-02-25 21:29:16 +0900

    feat: 내 정보 보기 API -> 회원 정보 API로 수정

--- src/main/java/com/hanamja/moa/api/controller/user/UserController.java
+++ src/main/java/com/hanamja/moa/api/controller/user/UserController.java
@@ -1,20 +1,14 @@
 package com.hanamja.moa.api.controller.user;
 
 import com.hanamja.moa.api.dto.user.response.UserInfoResponseDto;
-import com.hanamja.moa.api.entity.user.UserAccount.UserAccount;
 import com.hanamja.moa.api.service.user.UserService;
 import com.hanamja.moa.utils.s3.AmazonS3Uploader;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,11 +21,11 @@ public class UserController {
 
     private final AmazonS3Uploader amazonS3Uploader;
 
-    @Operation(summary = "내 정보 조회")
-    @GetMapping(value = "/my-info")
-    public ResponseEntity<UserInfoResponseDto> myInfo(@Parameter(hidden = true) @AuthenticationPrincipal UserAccount userAccount) {
+    @Operation(summary = "유저 정보 조회")
+    @GetMapping(value = "/{userId}/info")
+    public ResponseEntity<UserInfoResponseDto> myInfo(@PathVariable("userId") Long userId) {
 
-        UserInfoResponseDto responseDto = userService.getMyInfo(userAccount.getUserId());
+        UserInfoResponseDto responseDto = userService.getUserInfo(userId);
 
         return ResponseEntity.ok(responseDto);
     }
