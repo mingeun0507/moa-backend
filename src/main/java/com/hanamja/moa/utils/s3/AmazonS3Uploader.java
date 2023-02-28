331e7f7cdbf06665ec84d1dc894aa27a5aec5942
99winnmin <ojysep9987@gmail.com>
2023-02-28 23:33:26 +0900

    Fix: Image resize 하고 S3 저장하는 코드 rollback

--- src/main/java/com/hanamja/moa/utils/s3/AmazonS3Uploader.java
+++ src/main/java/com/hanamja/moa/utils/s3/AmazonS3Uploader.java
@@ -6,15 +6,10 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.UUID;
 
 @Slf4j
@@ -32,34 +27,9 @@ public class AmazonS3Uploader {
         objMeta.setContentType(multipartFile.getContentType());
         objMeta.setContentLength(multipartFile.getSize());
 
-        BufferedImage bufferedImage = ImageIO.read(multipartFile.getInputStream());
-
-        if (bufferedImage.getHeight() > 720 && bufferedImage.getWidth() > 720){
-            bufferedImage = resizeImage(bufferedImage, (int) (bufferedImage.getWidth()*0.5), (int) (bufferedImage.getHeight()*0.5));
-            multipartFile = convertBufferedImageToMultipartFile(bufferedImage);
-        }
-
         amazonS3.putObject(new PutObjectRequest(bucket, s3FileName, multipartFile.getInputStream(), objMeta)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
 
         return amazonS3.getUrl(bucket, s3FileName).toString();
-    }
-
-    //baeldung에서 가져온 이미지 리사이징 해주는 메서드
-    BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws Exception {
-        // resize에 들어가는 속성을 변경해서 여러 모드로 변경해줄수있다
-        return Scalr.resize(originalImage, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_EXACT, targetWidth, targetHeight, Scalr.OP_ANTIALIAS);
-    }
-
-    private MultipartFile convertBufferedImageToMultipartFile(BufferedImage image) {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        try {
-            ImageIO.write(image, "jpeg", out);
-        } catch (IOException e) {
-            log.error("IO Error", e);
-            return null;
-        }
-        byte[] bytes = out.toByteArray();
-        return new CustomMultipartFile(bytes, bytes.length);
     }
 }
