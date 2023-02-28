8f26ae318dd5a50538d148101fdeb47efebb6054
99winnmin <ojysep9987@gmail.com>
2023-03-01 06:40:33 +0900

    Feat: Front domain CORS 추가

--- src/main/java/com/hanamja/moa/config/CorsConfig.java
+++ src/main/java/com/hanamja/moa/config/CorsConfig.java
@@ -16,6 +16,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://moafrontend-env.eba-qtcfatkw.ap-northeast-2.elasticbeanstalk.com");
+        config.addAllowedOrigin("http://moa-frontend.s3-website.ap-northeast-2.amazonaws.com/");
+        config.addAllowedOrigin("https://d2xztibv89z47n.cloudfront.net/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("*");
