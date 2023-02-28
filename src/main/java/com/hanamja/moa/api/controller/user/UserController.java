e565ad2f06c9581cfabfe35acb11ee6bb2a61162
mingeun0507 <tqdlfkg@naver.com>
2023-02-28 14:31:57 +0900

    feat: 유저 정보 수정 API 구현

--- src/main/java/com/hanamja/moa/api/controller/user/UserController.java
+++ src/main/java/com/hanamja/moa/api/controller/user/UserController.java
@@ -1,6 +1,8 @@
 package com.hanamja.moa.api.controller.user;
 
+import com.hanamja.moa.api.dto.user.request.ModifyingUserInfoRequestDto;
 import com.hanamja.moa.api.dto.user.response.UserInfoResponseDto;
+import com.hanamja.moa.api.entity.user.UserAccount.UserAccount;
 import com.hanamja.moa.api.service.user.UserService;
 import com.hanamja.moa.utils.s3.AmazonS3Uploader;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,10 +10,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -28,5 +28,11 @@ public class UserController {
     @GetMapping(value = "/{userId}/info")
     public ResponseEntity<UserInfoResponseDto> myInfo(@PathVariable("userId") Long userId) {
         return ResponseEntity.ok().body(userService.getUserInfo(userId));
+    }
+
+    @Operation(summary = "마이페이지에서 유저 정보 변경")
+    @PutMapping(value = "/info")
+    public ResponseEntity<UserInfoResponseDto> modifyUserInfo(@AuthenticationPrincipal UserAccount userAccount, @RequestBody ModifyingUserInfoRequestDto modifyingUserInfoRequestDto) {
+        return ResponseEntity.ok().body(userService.modifyUserInfo(userAccount, modifyingUserInfoRequestDto));
     }
 }
