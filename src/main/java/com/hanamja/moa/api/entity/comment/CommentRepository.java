06d3ca6a5140ba01a77e53a51a83d78b1e9e5b8d
mingeun0507 <tqdlfkg@naver.com>
2023-03-12 21:55:22 +0900

    feat: Page<Comment> 를 리턴하는 메소드 구현

--- src/main/java/com/hanamja/moa/api/entity/comment/CommentRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/comment/CommentRepository.java
@@ -1,10 +1,14 @@
 package com.hanamja.moa.api.entity.comment;
 
 import com.hanamja.moa.api.entity.group.Group;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findTopByGroupOrderByCreatedAtDesc(Group group);
+
+    Page<Comment> findByGroup(Group group, Pageable pageable);
 }
