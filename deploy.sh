0578a7a39dc88bee23c5d05f9a8b2c1ec4d5f580
99winnmin <ojysep9987@gmail.com>
2023-03-13 00:21:05 +0900

    Fix: MOA 테스트 서버 CI/CD 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,8 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ec2-user/app/build/libs/moa-0.0.1-SNAPSHOT.jar > ~/nohup.out 2>&1 &
+nohup java -jar \
+      -Dspring.config.location=classpath:/application-test.yml \
+      -Dspring.profiles.active=test \
+      /home/ec2-user/app/build/libs/moa-0.0.1-SNAPSHOT.jar \
+      > ~/nohup.out 2>&1 &
