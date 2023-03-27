44c787c672cfc1ca989d9ec4abf2d005ba074531
mingeun0507 <tqdlfkg@naver.com>
2023-03-27 16:27:46 +0900

    fix: 스크립트 오류 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -22,6 +22,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=dev /home/ec2-user/nanumeal/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=dev /home/ec2-user/app/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
