ac145f9f141303e7dfb172ecabf7ab0c4ad577fb
mingeun0507 <tqdlfkg@naver.com>
2023-04-18 11:50:42 +0900

    feat: PostController 껍데기 파일 구현

---
+++ src/main/java/com/hanamja/moa/api/controller/post/PostController.java
@@ -0,0 +1,12 @@
+package com.hanamja.moa.api.controller.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/board")
+public class PostController {
+
+}
