3f805700ae7e5eea8bdaa49293f3c38b67e684c1
mingeun0507 <tqdlfkg@naver.com>
2023-04-18 12:15:47 +0900

    feat: PostService 및 구현체 껍데기 파일 생성

---
+++ src/main/java/com/hanamja/moa/api/service/post/PostService.java
@@ -0,0 +1,4 @@
+package com.hanamja.moa.api.service.post;
+
+public interface PostService {
+}
---
+++ src/main/java/com/hanamja/moa/api/service/post/PostServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hanamja.moa.api.service.post;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class PostServiceImpl implements PostService {
+
+}
