0458c6e103e5829436a09775d71013ebe7f7f05d
99winnmin <ojysep9987@gmail.com>
2023-05-03 19:10:59 +0900

    Feat: 게시판, 게시글 관련 JPA 추가
    "게시판 카테고리 추가 JPA 추가"
    "게시글 작성, 수정, 삭제, 좋아요, 북마크 JPA 추가"

--- src/main/java/com/hanamja/moa/api/entity/post_comment/PostCommentRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/post_comment/PostCommentRepository.java
@@ -1,6 +1,8 @@
 package com.hanamja.moa.api.entity.post_comment;
 
+import com.hanamja.moa.api.entity.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+    void deleteAllByPost(Post post);
 }
--- src/main/java/com/hanamja/moa/api/entity/post_image/PostImageRepository.java
+++ src/main/java/com/hanamja/moa/api/entity/post_image/PostImageRepository.java
@@ -3,4 +3,5 @@ package com.hanamja.moa.api.entity.post_image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+    void deleteAllByPost_Id(Long postId);
 }
