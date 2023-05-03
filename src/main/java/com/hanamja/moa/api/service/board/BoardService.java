2bceac6182eb1b99a4993064dc226da1e56b0e05
99winnmin <ojysep9987@gmail.com>
2023-05-03 19:08:33 +0900

    Feat: 게시판 카테고리 추가 요청 비지니스 로직 추가

--- src/main/java/com/hanamja/moa/api/service/board/BoardService.java
+++ src/main/java/com/hanamja/moa/api/service/board/BoardService.java
@@ -1,15 +1,17 @@
 package com.hanamja.moa.api.service.board;
 
+import com.hanamja.moa.api.dto.board.request.NewBoardCategoryRequestDto;
 import com.hanamja.moa.api.dto.board.response.DepartmentBoardInfoResponseDto;
-import com.hanamja.moa.api.entity.department.Department;
+import com.hanamja.moa.api.entity.board.Board;
 import com.hanamja.moa.api.entity.user.UserAccount.UserAccount;
 
 public interface BoardService {
 
     /* resolve 함수 - 객체의 id를 validate한 뒤 resolve 해오는 내부 함수들 */
-    Department resolveDepartmentById(UserAccount userAccount);
-
+    Board resolveBoardById(Long boardId);
 
     /* Controller에서 사용할 함수들 */
     DepartmentBoardInfoResponseDto getBoardInfoByDepartment(UserAccount userAccount);
+
+    void makeNewBoardCategory(UserAccount userAccount, NewBoardCategoryRequestDto newBoardCategoryRequestDto);
 }
