cf522eadd221d1dd5cc35b6acdc75142529997f0
99winnmin <ojysep9987@gmail.com>
2023-05-13 00:04:45 +0900

    Feat: 게시글 수정 요청 dto

---
+++ src/main/java/com/hanamja/moa/api/dto/post/request/BoardPostEditRequestDto.java
@@ -0,0 +1,18 @@
+package com.hanamja.moa.api.dto.post.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardPostEditRequestDto {
+    private Long postId;
+    private String title;
+    private Long categoryId;
+    private String content;
+    private List<String> images;
+
+}
