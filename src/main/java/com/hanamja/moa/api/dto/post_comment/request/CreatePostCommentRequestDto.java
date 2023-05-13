fc4f3771e3b5991ac5e130c8646981836a5a32c2
bjih1999 <bjih1999@naver.com>
2023-05-14 03:40:39 +0900

    fix: request dto 객체 생성 못하는 버그 수정

--- src/main/java/com/hanamja/moa/api/dto/post_comment/request/CreatePostCommentRequestDto.java
+++ src/main/java/com/hanamja/moa/api/dto/post_comment/request/CreatePostCommentRequestDto.java
@@ -1,14 +1,11 @@
 package com.hanamja.moa.api.dto.post_comment.request;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreatePostCommentRequestDto {
 
     private Long parentCommentId;
